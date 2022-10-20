5409aeb537f7e86af5a0e63be0a8df4115ee0a81
Matthew Pahl <pahlmc@gmail.com>
2022-10-19 20:47:03 -0400

    first commit

---
+++ convert2pdf.R
@@ -0,0 +1,14 @@
+# Knit the HTML version
+rmarkdown::render("cv_render.Rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "matt_pahl_cv.html")
+
+# Knit the PDF version to temporary html location
+# use this to replace links with footnotes
+
+
+
+pagedown::chrome_print(input = "matt_pahl_cv.html",
+                       output = "matt_pahl_cv.pdf",
+                       verbose = TRUE
+                      )
